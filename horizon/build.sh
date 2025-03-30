ef8f54011f9ab42a37d3393a6552d395e4559842
emperornintri <emperornintri@gmail.com>
2025-03-30 05:26:36 +0300

    [FIX] Add Visual Studio Code files to .gitignore.

--- horizon/build.sh
+++ horizon/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-SRC_DIR="source"
 INC_DIR="include"
+SRC_DIR="source"
+MAIN="main.c"
 OUT="program"
+START="startHorizon"
 
 SRCS=$(find "$SRC_DIR" -name "*.c")
 
-gcc -I "$INC_DIR" -nostdlib -e startHorizon main.c $SRCS -o "$OUT"
+gcc -I "$INC_DIR" -nostdlib -e "$START" "$MAIN" $SRCS -o "$OUT"
 
-echo "Build completed: $OUT"
+echo "Build completed: \"$OUT\"."
